ce669612f757ecd21ac9c844a0814e55883a084e
Emily Marx <emilykmarx@alumni.stanford.edu>
2023-05-12 12:59:17 -0700

    generate random topo, set up ifaces and static routes to and from one dest. Ping all routes, trace one.

---
+++ go_expts/deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -xe
+
+kind delete cluster --name=kne
+kne deploy deploy/kne/kind-bridge.yaml
+
+set +e # ok if exists
+kubectl create namespace srlinux-controller
+set -e
+kubectl create -n srlinux-controller \
+    secret generic srlinux-licenses --from-file=all.key=licenses_do_not_push.key \
+    --dry-run=client --save-config -o yaml | \
+    kubectl apply -f -
+
+kind load docker-image ghcr.io/nokia/srlinux:23.3.1 --name kne
---
+++ go_expts/gen_topo_graph.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+"""
+"""
+
+import argparse
+import networkx as nx
+import matplotlib.pyplot as plt
+from itertools import combinations, groupby
+import random
+import json
+
+filepath = "go_expts/"
+
+def gnp_random_connected_graph(n, p, seed=None):
+  random.seed(seed) # unseeded if seed=None
+  edges = combinations(range(n), 2)
+  G = nx.Graph()
+  G.add_nodes_from(range(n))
+  for _, node_edges in groupby(edges, key=lambda x: x[0]):
+      node_edges = list(node_edges)
+      random_edge = random.choice(node_edges)
+      G.add_edge(*random_edge)
+      for e in node_edges:
+          if random.random() < p:
+              G.add_edge(*e)
+  return G
+
+def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument("-n", "--nodes", required=True, type=int)
+  parser.add_argument("-p", "--link-probability", required=True, type=float)
+  args = parser.parse_args()
+  # Note draw is not seeded (maybe there's a way to do that)
+  seed = 1
+  G = gnp_random_connected_graph(args.nodes, args.link_probability, seed)
+
+  plt.figure(figsize=(8,5))
+  nx.draw(G, node_color='lightblue',
+          with_labels=True,
+          node_size=500)
+  plt.savefig(filepath + 'topo.png')
+
+  # {src: {dest: [path], ...}, ...}
+  # paths = nx.shortest_path(G)
+  # Only do paths to one dest for now
+  paths = nx.shortest_path(G, target=args.nodes-1)
+  with open(filepath + "topo.json", "w") as f:
+    f.write(f"{{\"Graph\": {json.dumps(nx.node_link_data(G))},\n")
+    f.write(f"\"Paths\": {json.dumps(paths)}}}")
+
+  #plt.show()
+
+
+
+if __name__ == "__main__":
+  main()
